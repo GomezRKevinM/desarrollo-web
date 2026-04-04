77d1a7db662f5034d583e513d9f704539cb93fd9
kevin-desktop <kgomezr2@unicartagena.edu.co>
2026-04-04 18:09:10 -0500

    renamed: GetAllUSerUseCase to GetAllUsersUseCase

--- crud-usuarios/Application/Ports/In/GetAllUsersUseCase.php
+++ crud-usuarios/Application/Ports/In/GetAllUsersUseCase.php
@@ -7,7 +7,7 @@ namespace App\crud_usuarios\Application\Ports\In;
 use App\crud_usuarios\Application\Services\Dto\Queries\GetAllUsersQuery;
 use App\crud_usuarios\Domain\Models\UserModel;
 
-interface GetAllUserUseCase
+interface GetAllUsersUseCase
 {
     /** @return UserModel[] */
     public function execute(GetAllUsersQuery $query): array;
