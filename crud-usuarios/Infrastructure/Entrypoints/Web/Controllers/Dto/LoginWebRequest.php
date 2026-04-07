04e0511b6bf91afc28e7061301a2ab2bf1572544
kevin-aspire <kgomezr2@unicartagena.edu.co>
2026-04-06 19:37:09 -0500

    feat: module controller already - Kevin Gomez

---
+++ crud-usuarios/Infrastructure/Entrypoints/Web/Controllers/Dto/LoginWebRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\crud_usuarios\Infrastructure\Entrypoints\Web\Controllers\Dto;
+
+final readonly class LoginWebRequest
+{
+    public function __construct(
+        private string $email,
+        private string $password,
+    ) {}
+
+    public function getEmail(): string    { return $this->email; }
+    public function getPassword(): string { return $this->password; }
+
+}
---
+++ crud-usuarios/Infrastructure/Entrypoints/Web/Controllers/Dto/UpdateUserWebRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\crud_usuarios\Infrastructure\Adapters\Persistence\MySQL\Dto;
+
+final readonly class UpdateUserWebRequest
+{
+    public function __construct(
+        private string $id,
+        private string $name,
+        private string $email,
+        private string $password,
+        private string $role,
+        private string $status,
+    ) {}
+
+    public function getId(): string       { return $this->id; }
+    public function getName(): string     { return $this->name; }
+    public function getEmail(): string    { return $this->email; }
+    public function getPassword(): string { return $this->password; }
+    public function getRole(): string     { return $this->role; }
+    public function getStatus(): string   { return $this->status; }
+}
